b6d59120e47644f660424a803498c9a5ba34c08d
Iñigo Marin <inigomaringarcia@gmail.com>
2020-06-14 12:45:09 +0200

    Open developer tools in monitor 3

--- config.h
+++ config.h
@@ -48,6 +48,8 @@ static const Rule rules[] = {
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
 	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
 	{ "St",       NULL,       NULL,       	    0,            0,           1,         0,        -1 },
+	{ "Postman",  NULL,       NULL,       	    1 << 2,       0,           1,         0,         0 },
+	{ "Spring Tool Suite 4",  NULL,       NULL,       	    1 << 2,       0,           1,         0,         -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
 	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
